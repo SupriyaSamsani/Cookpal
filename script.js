c21b4968ecfe02bea07edbae831405aeaa44928f
SupriyaSamsani <supriyasamsani@gmail.com>
2023-11-05 01:25:48 +0530

    prototype-1

---
+++ script.js
@@ -0,0 +1,161 @@
+const menu = [
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dmVnJTIwZGVsaWdodHxlbnwwfHwwfHx8MA%3D%3D",
+        "time": "30 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Chicken Grill",
+        "imageSrc": "https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dmVnJTIwZGVsaWdodHxlbnwwfHwwfHx8MA%3D%3D=",
+        "time": "45 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dmVnJTIwZGVsaWdodHxlbnwwfHwwfHx8MA%3D%3D",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.1
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1593967858208-67ddb5b4c406?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dmVnJTIwZGVsaWdodHxlbnwwfHwwfHx8MA%3D%3D=",
+        "time": "60 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.7
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://media.istockphoto.com/id/175028181/photo/fillet-of-salmon-with-vegetable.jpg?s=2048x2048&w=is&k=20&c=ynco16RV3dVAUHR6qAUDuXTQY7bPL0QTOigDSK0np5I=",
+        "time": "50 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    {
+        "name": "Tomato Pasta",
+        "imageSrc": "https://media.istockphoto.com/id/1325150602/photo/waffles-with-fried-chicken.jpg?s=2048x2048&w=is&k=20&c=sZIE7MloFECSXzXsxhSt4iAqqKgfrcEXiXypp3WG6Kc=",
+        "time": "35 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.0
+    },
+    {
+        "name": "Vegan Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1628294896516-344152572ee8?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNoaWNrZW4lMjBncmlsbHxlbnwwfHwwfHx8MA%3D%3D",
+        "time": "20 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 3.9
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1628294896516-344152572ee8?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNoaWNrZW4lMjBncmlsbHxlbnwwfHwwfHx8MA%3D%3D=",
+        "time": "55 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.3
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://media.istockphoto.com/id/1496031397/photo/close-up-of-cutting-mushrooms.jpg?s=2048x2048&w=is&k=20&c=UJarKoLtQrpb8dZTC9oEqTD2J1v14pDatrvjeVGxlYo=",
+        "time": "45 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.5
+    },
+    {
+        "name": "Burger",
+        "imageSrc": "https://media.istockphoto.com/id/998309062/photo/burger-with-beef-and-cheese.jpg?s=2048x2048&w=is&k=20&c=WiewZ2pC0tP1-NfjpxTkWYXVCBotqrIzWXc2HkAewkk=",
+        "time": "30 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.2
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://media.istockphoto.com/id/1207328341/photo/indian-cuisine-tofu-butter-masala-panner-with-roti-closeup-in-a-pan-horizontal-top-view.jpg?s=2048x2048&w=is&k=20&c=ZMP9usctRT7PJdHChSuiKAnjP5jx3TlHX5YJ6rtIonc=",
+        "time": "40 min",
+        "type": "veg",
+        "isLiked": false,
+        "rating": 4.4
+    },
+    {
+        "name": "Russian Salad",
+        "imageSrc": "https://images.unsplash.com/photo-1628294896516-344152572ee8?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNoaWNrZW4lMjBncmlsbHxlbnwwfHwwfHx8MA%3D%3D=",
+        "time": "70 min",
+        "type": "non-veg",
+        "isLiked": false,
+        "rating": 4.6
+    },
+    
+]
+const searchInput = document.getElementById("search");
+const menuCatalog = document.getElementById("menu");
+const aboveButton = document.getElementById("ratingup");
+const belowButton = document.getElementById("ratingbelow");
+const allRecipe = document.getElementById("all-recipe");
+const vegetarian = document.getElementById("veg");
+const nonveg = document.getElementById("non-veg");
+
+
+document.getElementById("all-recipe").addEventListener("click", () =>{
+    displayRecipes(menu);
+});
+document.getElementById("veg").addEventListener("click", () => {
+    const vegRecipes = menu.filter(recipe => recipe.type === "veg");
+    displayRecipes(vegRecipes);
+});
+
+document.getElementById("non-veg").addEventListener("click", () => {
+    const nonVegRecipes = menu.filter(recipe => recipe.type === "non-veg");
+    displayRecipes(nonVegRecipes);
+});
+
+//ratings
+aboveButton.addEventListener("click", () => {
+    const above4Rating = menu.filter(recipe => recipe.rating >= 4);
+    displayRecipes(above4Rating);
+});
+
+belowButton.addEventListener("click", () => {
+    const below4Rating = menu.filter(recipe => recipe.rating < 4);
+    displayRecipes(below4Rating);
+});
+
+
+// search function
+searchInput.addEventListener("input", filterRecipes);
+function filterRecipes() {
+    const searchText = searchInput.value.toLowerCase();
+    const filteredRecipes = menu.filter(recipe => recipe.name.toLowerCase().includes(searchText));
+    displayRecipes(filteredRecipes);
+}
+
+function displayRecipes(recipes) {
+    const menuCatalog = document.getElementById("menu"); 
+
+    menuCatalog.innerHTML = ""; 
+
+    recipes.forEach(recipe => {
+        const card = document.createElement("div");
+        card.className = "recipe-card";
+        card.innerHTML = `
+            <img src="${recipe.imageSrc}" alt="${recipe.name}">
+            <h3>${recipe.name}</h3>
+            <p>Type: ${recipe.type}</p>
+            <p>Time: ${recipe.time}</p>
+            <p>Rating: ${recipe.rating}</p>
+           
+        `;
+        menuCatalog.appendChild(card); 
+    });
+}
+displayRecipes(menu);
